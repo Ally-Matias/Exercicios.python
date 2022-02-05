a1662a83200cb21210f8ebe489f2536d7cd4eb93
Ally-Matias <98532868+Ally-Matias@users.noreply.github.com>
2022-02-05 11:09:53 -0300

    Create Categoria de natação.py
    Numa escola de natação, existe diversas categoria que uma criança pode ser inserida de acordo com sua idade. As categorias existentes estão na tabela abaixo:
    
    Categorias
    Idade	Categoria
    Até 3 anos	fralda
    Até 5 anos	baby
    Até 7 anos	kid 
    Até 12 anos	juvenil
    Até 19 anos	jovem
    Acima de 19 anos	não permitido

---
+++ Estruturas condicionais/Categoria de natação.py
@@ -0,0 +1,14 @@
+num = int(input(""))
+
+if num <= 3:
+	print("fralda")
+elif num > 3 and num <= 5:
+	print("baby")
+elif num > 5 and num <= 7:
+	print("kid")
+elif num > 7 and num <= 12:
+	print("juvenil")
+elif num > 12 and num <= 19:
+	print("jovem")
+elif num > 19:
+	print("nao permitido")
